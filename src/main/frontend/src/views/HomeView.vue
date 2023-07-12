cf09cfd655da9a5805b1958e3d8921c4a136cde2
David Daza <drdaza@gmail.com>
2023-07-12 07:16:06 +0200

    get in frontend

--- src/main/frontend/src/views/HomeView.vue
+++ src/main/frontend/src/views/HomeView.vue
@@ -1,7 +1,32 @@
 <script setup>
+import { useQuery} from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+import { computed, watchEffect } from 'vue';
+
+
+const ALL_AUTHORS_QUERY = gql`
+      query {
+        findAllAuthors{
+          id
+          name
+          age
+      }
+      }
+`
+const {result} = useQuery(ALL_AUTHORS_QUERY)
+const authors = computed(()=>
+  result.value?.findAllAuthors ?? []
+)
+
+watchEffect(()=>{
+console.log(result)
+})
+
 </script>
 
 <template>
   <main>
+    <ul>
+    <li v-for="author in authors" :key="author.id">{{ author.name }}</li></ul>
   </main>
 </template>

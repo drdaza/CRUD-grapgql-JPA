8c1908e7c799ff8d73c1d151c5dd9ccf37de415e
David Daza <drdaza@gmail.com>
2023-07-12 19:25:29 +0200

    added basic query and mutation elements in front

--- src/main/frontend/src/views/HomeView.vue
+++ src/main/frontend/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
-import { useQuery} from '@vue/apollo-composable'
+import { useQuery, useMutation} from '@vue/apollo-composable'
 import gql from 'graphql-tag'
-import { computed, watchEffect } from 'vue';
+import { computed, ref } from 'vue';
 
+const name = ref('')
+const age = ref(0)
 
 const ALL_AUTHORS_QUERY = gql`
       query {
@@ -13,19 +15,46 @@ const ALL_AUTHORS_QUERY = gql`
       }
       }
 `
+
+const CREATE_AUTHOR_MUTATION = gql`
+      mutation createAuthor($name: String! $age: Int){
+        createAuthor (name: $name age: $age) {
+          id
+          name
+          age
+        }
+      }
+`
 const {result} = useQuery(ALL_AUTHORS_QUERY)
 const authors = computed(()=>
   result.value?.findAllAuthors ?? []
 )
 
-watchEffect(()=>{
-console.log(result)
-})
+const {mutate: createAuthor} = useMutation(CREATE_AUTHOR_MUTATION, () => ({
+  variables: {
+    name: name.value,
+    age: age.value
+  }, 
+  update: (cache, {data: {createAuthor}}) =>{
+    let data = cache.readQuery({query: ALL_AUTHORS_QUERY})
+    data = {
+      ...data,
+      findAllAuthors: [
+        ...data.findAllAuthors,
+        createAuthor,
+      ],
+    }
+    cache.writeQuery({query: ALL_AUTHORS_QUERY, data})
+  }
+}))
 
 </script>
 
 <template>
   <main>
+    <input type="text" v-model="name">
+    <input type="number" v-model="age">
+    <button @click="createAuthor()">test</button>
     <ul>
     <li v-for="author in authors" :key="author.id">{{ author.name }}</li></ul>
   </main>

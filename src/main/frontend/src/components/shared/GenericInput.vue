5c0f9a1c4d1d0a51a2d359b8c8099ce2d4a5d0b2
David Daza <drdaza18@gmail.com>
2023-07-29 18:03:40 +0200

    adding new validations for input of type number, necessary refactoring, and excessive logic in a method.

--- src/main/frontend/src/components/shared/GenericInput.vue
+++ src/main/frontend/src/components/shared/GenericInput.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive, computed } from 'vue';
+import { ref, reactive, computed,toRef } from 'vue';
 // props
 const props = defineProps({
     value: {
@@ -21,28 +21,65 @@ const valueOfInput = ref(null)
 const numberValue = reactive({
     symbol: '',
     intValue: '',
-    decimalSymbol: '.',
-    decimalValue: ''
+    decimalSymbol: '',
+    decimalValue: '',
+    numericValue: ''
 })
+const NEGATIVE_SYMBOL = '-'
+const POSITIVE_SYMBOL = '+'
+const EMPTY_SYMBOL = ''
+const DECIMAL_SYMBOL = '.'
+
 
 // methods
 const inputHandler = (event) => {
-    const validExpressions = [/^-?\d+$/, /^-?\d+(\.\d+)?(,\d+)?$/]
+    const validExpressions = [/^[-+]?[0-9]*[,.]?[0-9]?[0-9]+$/, /^[-+]?[0-9]$/, /^[-+]?[0-9]*[,.]$/]
     const lastValueInput = event.data
-    let eventHandlerValue = event.target.value
+    let inputValue = event.target.value
 
     const emitMapOptions = {
         'text': ()=>{
-            valueOfInput.value = parseValue(event.target.value)
+            valueOfInput.value = event.target.value
         },
         'number': ()=>{
-            console.log(validExpressions.map(exp => exp.test(event.data)));
-            if (Number.isNaN(parseFloat(event.data))) {
+            debugger
+            if (lastValueInput === NEGATIVE_SYMBOL) {
+                numberValue.symbol = NEGATIVE_SYMBOL
+            }
+
+            if (lastValueInput === POSITIVE_SYMBOL || !inputValue.includes(NEGATIVE_SYMBOL)){
+                numberValue.symbol = POSITIVE_SYMBOL
+            }
+
+            if (lastValueInput === DECIMAL_SYMBOL && (valueOfInput.value === null || valueOfInput.value === NEGATIVE_SYMBOL || valueOfInput.value === EMPTY_SYMBOL) ) {
+                inputValue = numberValue.symbol === NEGATIVE_SYMBOL ? `${numberValue.symbol}0.` : '0.'
+                valueOfInput.value = inputValue
+                return
+            }
+
+            const inputValueHasMoreThanOneDecimalSymbol = inputValue.split('').filter(symbol =>  symbol === DECIMAL_SYMBOL).length > 1
+            
+            if (inputValueHasMoreThanOneDecimalSymbol && lastValueInput === DECIMAL_SYMBOL) {
+                event.target.value = valueOfInput.value
+                inputValue = valueOfInput.value
+                return
+            }
+
+            inputValue = inputValue.replace(POSITIVE_SYMBOL, EMPTY_SYMBOL) 
+            inputValue = inputValue.replace(NEGATIVE_SYMBOL, EMPTY_SYMBOL)
+            
+            if (inputValue !== EMPTY_SYMBOL && !validExpressions.find(regex => regex.test(inputValue))) {
                 event.target.value = valueOfInput.value
                 return
             }
-            // parseValue(event.target.value)
-            valueOfInput.value = parseValue(event.target.value)
+
+            numberValue.numericValue = inputValue
+            if(numberValue.numericValue === EMPTY_SYMBOL) {
+                valueOfInput.value = numberValue.symbol === NEGATIVE_SYMBOL ? numberValue.symbol : EMPTY_SYMBOL
+                return
+            }
+
+            valueOfInput.value = numberValue.symbol === NEGATIVE_SYMBOL ? parseFloat(`${numberValue.symbol}${numberValue.numericValue}`) : parseFloat(inputValue)
         }
     }
     emitMapOptions[props.type]()

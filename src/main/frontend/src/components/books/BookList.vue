db7dbd8981eb1df0808fe219f5e4a011e0d51a32
David Daza <drdaza18@gmail.com>
2023-08-06 15:26:40 +0200

    litle changes

--- src/main/frontend/src/components/books/BookList.vue
+++ src/main/frontend/src/components/books/BookList.vue
@@ -7,22 +7,32 @@ import BookListItem from './BookListItem.vue'
 const bookStore = useBookStore()
 
 onBeforeMount(() => {
-  bookStore.findAllBooks()  
+  bookStore.findAllBooks()
 })
 
 const emits = defineEmits(['emitBookInfoForEdit'])
 
-const emitBookInfo = (event)=>{
+const emitBookInfo = (event) => {
   const bookForEdit = bookStore.allBooks.find(book => book.id && book.id === event)
   emits('emitBookInfoForEdit', bookForEdit)
 }
 // Hacer un script para rellenar tanto authors como books de forma automatica, probablemente con python o con una libreria de js, si es que existe alguna y lo hace de buena manera
 </script>
 <template>
-    <div class="list-wrapper">
-        <BookListItem v-for="book of bookStore.allBooks" :book="book" @edit-book-emit="emitBookInfo"/>
+  <div class="list-wrapper">
+    <div class="list-item-wrapper" v-for="book of bookStore.allBooks">
+      <BookListItem  :book="book" @edit-book-emit="emitBookInfo" />
     </div>
+  </div>
 </template>
 <style lang="scss" scoped>
-    
+@use '../../assets/sass/mixins' as *;
+
+.list-wrapper {
+  @include flexDisplay(row, center, space-evenly, 100%);
+  flex-wrap: wrap;
+  .list-item-wrapper {
+    width: 30%;
+  }
+}
 </style>
